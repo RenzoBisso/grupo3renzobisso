7ea5fd905d29070cd98f43f1c86b28ab39b2de4b
IvanAlejoP <ialejoperalta@gmail.com>
2025-04-09 22:04:30 -0300

    xd

--- programacion2-2025/02-trabajoPractico-Listas/src/tp_2_listas.c
+++ programacion2-2025/02-trabajoPractico-Listas/src/tp_2_listas.c
@@ -2,7 +2,40 @@
 #include "..\headers\tp_2_listas.h"
 
 //PUNTO 2
-//A
+//A Y B
+Lista verElementosQueNoSeRepiten(Lista l1, Lista l2){
+    Lista lresultado = l_crear();
+
+    Iterador iter1 = iterador(l1);
+
+    TipoElemento x = te_crear(0);
+    TipoElemento y = te_crear(0);
+
+    bool bandera;
+
+    while(hay_siguiente(iter1)){
+        x = siguiente(iter1);
+
+        Iterador iter2 = iterador(l2);
+
+        while(hay_siguiente(iter2)){
+            y = siguiente(iter2);
+            if(x->clave != y->clave){
+                bandera = false;
+            }
+        }
+        if(!bandera){
+            l_agregar(lresultado, x);
+        }
+
+        bandera = true;
+        free(iter2);
+    }
+
+    return lresultado;
+}
+
+//C
 Lista verElementosRepetidos(Lista l1, Lista l2){
     Lista lresultado = l_crear();
 
@@ -35,35 +68,9 @@ Lista verElementosRepetidos(Lista l1, Lista l2){
     return lresultado;
 }
 
-//B
-Lista verElementosQueNoSeRepiten(Lista l1, Lista l2){
-    Lista lresultado = l_crear();
+//D
+float promedio(Lista l1){}
 
-    Iterador iter1 = iterador(l1);
 
-    TipoElemento x = te_crear(0);
-    TipoElemento y = te_crear(0);
 
-    bool bandera;
 
-    while(hay_siguiente(iter1)){
-        x = siguiente(iter1);
-
-        Iterador iter2 = iterador(l2);
-
-        while(hay_siguiente(iter2)){
-            y = siguiente(iter2);
-            if(x->clave != y->clave){
-                bandera = false;
-            }
-        }
-        if(!bandera){
-            l_agregar(lresultado, x);
-        }
-
-        bandera = true;
-        free(iter2);
-    }
-
-    return lresultado;
-}
