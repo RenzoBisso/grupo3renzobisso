7edb84cd53a846c0a18444b86701c4829b1cef12
IvanAlejoP <ialejoperalta@gmail.com>
2025-04-14 21:28:48 -0300

    Agregue los puntos que hice

--- programacion2-2025/02-trabajoPractico-Listas/src/tp_2_listas.c
+++ programacion2-2025/02-trabajoPractico-Listas/src/tp_2_listas.c
@@ -2,7 +2,7 @@
 #include "..\headers\tp_2_listas.h"
 
 //PUNTO 2
-//A Y B
+//A-B
 Lista verElementosQueNoSeRepiten(Lista l1, Lista l2){
     Lista lresultado = l_crear();
 
@@ -69,8 +69,186 @@ Lista verElementosRepetidos(Lista l1, Lista l2){
 }
 
 //D
-float promedio(Lista l1){}
+float promedio(Lista l1){
+    Iterador iter = iterador(l1);
+    TipoElemento X = te_crear(0);
+    float suma = 0;
+    float cant = 0;
 
+    while(hay_siguiente(iter)){
+        suma += siguiente(iter)->clave;
+        cant++;
+    }
+    float resultado = suma / cant;
+    return resultado;
+}
 
+Resultados promedioAmbasListas(Lista l1, Lista l2){
+    Resultados resultado;
+    resultado.resultado1 = promedio(l1);
+    resultado.resultado2 = promedio(l2);
+    return resultado;
+}
+
+//E
+ResultadoValorMinimo valorMaximo(Lista l1, Lista l2){ //EN REALIDAD BUSCA EL VALOR MÍNIMO, PERO ASÍ ESTÁ DECLARADO EN EL .h
+    ResultadoValorMinimo resultado;
+
+    Iterador iter1 = iterador(l1);
+    Iterador iter2 = iterador(l2);
+
+    TipoElemento X = te_crear(0);
+
+    int actual1 = siguiente(iter1)->clave;
+    int i = 1;
+    int pos1;
+
+    while(hay_siguiente(iter1)){
+        X = siguiente(iter1);
+        if(X->clave < actual1){
+            actual1 = X->clave;
+            pos1 = i;
+        }
+        i++;
+    }
+
+    int actual2 = siguiente(iter2)->clave;
+    i = 1;
+    int pos2;
+
+    while(hay_siguiente(iter2)){
+        X = siguiente(iter2);
+        if(X->clave < actual2){
+            actual2 = X->clave;
+            pos2 = i;
+        }
+        i++;
+    }
+
+    if(actual2 < actual1){
+        resultado.pos = pos1;
+        resultado.valor = actual1;
+    }
+    else{
+        resultado.pos_2 = pos2;
+        resultado.valor_2 = actual2;
+    }
+    return resultado;
+}
+
+//
+//
+//
+//
+//
+//PUNTO 3
+ResultadosMul multiplo(Lista l1, Lista l2){
+    Lista l3 = l_crear();
+    
+    Iterador iter1 = iterador(l1);
+    Iterador iter2 = iterador(l2);
+    Iterador iter3 = iterador(l3);
+
+    TipoElemento X = te_crear(0);
+    TipoElemento Y = te_crear(0);
+
+    ResultadosMul resultado;
+
+    int suma = 0;
+    bool bandera = true;
+    int actual;
+
+    while(hay_siguiente(iter1) && hay_siguiente(iter2)){
+        X = siguiente(iter1);
+        Y = siguiente(iter2);
+        if(Y->clave == 0 || es_decimal((float)X->clave / (float)Y->clave)){
+            resultado.esMultiplo = false;
+            resultado.escalar = false;
+            resultado.numEscalar = 0;
+            return resultado;
+        }
+        else{
+            l_agregar(l3, te_crear(X->clave / Y->clave));
+        }
+    }
+    
+    X = siguiente(iter3);
+    actual = X->clave;
+
+    while(hay_siguiente(iter3)){
+        X = siguiente(iter3);
+        if(actual != X->clave){
+            bandera = false;
+            break;
+        }
+        actual = X->clave;
+    }
+    if(bandera){
+        resultado.esMultiplo = true;
+        resultado.escalar = true;
+        resultado.numEscalar = X->clave;
+        return resultado;
+    }
+    else{
+        resultado.esMultiplo = true;
+        resultado.escalar = false;
+        resultado.numEscalar = 0;
+        return resultado;
+    }
+    free(iter1);
+    free(iter2);
+    free(iter3);
+    free(X);
+    free(Y);
+}
+
+//PUNTO 4
+int CompararListas(Lista l1, Lista l2){
+    Iterador iter1 = iterador(l1);
+    Iterador iter2 = iterador(l2);
+
+    int sumal1 = 0;
+    int sumal2 = 0;
+
+    while(hay_siguiente(iter1)){
+        sumal1 += siguiente(iter1)->clave;
+    }
+    
+    while(hay_siguiente(iter2)){
+        sumal2 += siguiente(iter2)->clave;
+    }
+
+    if(sumal1 > sumal2){
+        return 1;
+    }
+    else if(sumal1 == sumal2){
+        return 0;
+    }
+    else{
+        return 2;
+    }
+}
+
+//PUNTO 5 TE LA DEBO
+
+//PUNTO 6
+bool esSublista(Lista l1, Lista l2){
+    Iterador iter1 = iterador(l1);
+    Iterador iter2 = iterador(l2);
+
+    TipoElemento X = te_crear(0);
+    TipoElemento Y = te_crear(0);
+
+    while(hay_siguiente(iter2)){
+        X = siguiente(iter2);
+        while(hay_siguiente(iter1)){
+            Y = siguiente(iter1);
+            if(X->clave != Y->clave){
+                return false;
+            }
+        }
+    }
+    return true;
+}
 
 
