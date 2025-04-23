e8511c9471fa7481224b80b7c30e2aa09f8a1d84
IvanAlejoP <ialejoperalta@gmail.com>
2025-04-23 18:42:35 -0300

    ej5,6,7

--- programacion2-2025/03-trabajoPractico-Pilas/src/main.c
+++ programacion2-2025/03-trabajoPractico-Pilas/src/main.c
@@ -1,10 +1,19 @@
 #include "..\headers\utilidades.h"
+#include "..\headers\tp_3_pilas.h" //TEMPORAL
 
 int main(){
 
     srand(time(NULL));
 
-    menu();
+    //menu();
+
+    char* texto = malloc(100 * sizeof(char));
+
+    texto = p_ej4_cambiarbase(10, 2);
+
+    printf("%s", texto);
+
+    pausa();
 
     return 0;
 }
