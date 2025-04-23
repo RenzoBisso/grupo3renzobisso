11d3581644652ca517932e93ca9609b670e0d582
Renzo Bisso <renzo_bisso@outlook.com>
2025-04-23 18:44:01 -0300

    ejercicio hexa

--- programacion2-2025/03-trabajoPractico-Pilas/src/main.c
+++ programacion2-2025/03-trabajoPractico-Pilas/src/main.c
@@ -1,4 +1,6 @@
 #include "..\headers\utilidades.h"
+#include "..\headers\tp_3_pilas.h"
+
 
 int main(){
 
--- programacion2-2025/03-trabajoPractico-Pilas/src/tp_3_pilas.c
+++ programacion2-2025/03-trabajoPractico-Pilas/src/tp_3_pilas.c
@@ -77,13 +77,13 @@ Pila p_ej2_intercambiarposiciones(Pila p, int pos1, int pos2) {
 	}
 	int posActual = 1;
 	Pila pAux = p_crear();
-	TipoElemento elemento1 = te_crear(NULL);
-	TipoElemento elemento2 = te_crear(NULL);
+	TipoElemento elemento1 = te_crear(0);
+	TipoElemento elemento2 = te_crear(0);
 
 
 	//buscamos los elementos en las posiciones dadas y guardamos en la pila auxiliar
 	while (p_tope(p) != NULL) {
-		TipoElemento elementoAux = te_crear(NULL);
+		TipoElemento elementoAux = te_crear(0);
 		if (posActual == pos1) {
 			elemento1 = p_desapilar(p);
 			p_apilar(pAux, elemento1);
@@ -102,7 +102,7 @@ Pila p_ej2_intercambiarposiciones(Pila p, int pos1, int pos2) {
 	}
 	//Vamos restando el contador hasta que llegamos a la posicion dada y apilamos el valor correspondiente
 	while (p_tope(pAux) != NULL) {
-		TipoElemento elementoAux = te_crear(NULL);
+		TipoElemento elementoAux = te_crear(0);
 		if (posActual == pos1) {
 			p_desapilar(pAux);
 			p_apilar(p, elemento2);
@@ -130,12 +130,12 @@ Pila p_ej2_duplicar(Pila p) {
 	Pila pAux = p_crear();
 
 	while (p_tope(p) != NULL) {
-		TipoElemento elementoAux = te_crear(NULL);
+		TipoElemento elementoAux = te_crear(0);
 		elementoAux = p_desapilar(p);
 		p_apilar(pAux, elementoAux);
 	}
 	while (p_tope(pAux) != NULL) {
-		TipoElemento elementoAux = te_crear(NULL);
+		TipoElemento elementoAux = te_crear(0);
 		elementoAux = p_desapilar(pAux);
 		p_apilar(p, elementoAux);
 		p_apilar(p2, elementoAux);
@@ -151,13 +151,13 @@ int p_ej2_cantidadelementos(Pila p) {
 	Pila pAux = p_crear();
 	int count = 0;
 	while (p_tope(p) != NULL) {
-		TipoElemento elementoAux = te_crear(NULL);
+		TipoElemento elementoAux = te_crear(0);
 		elementoAux = p_desapilar(p);
 		p_apilar(pAux, elementoAux);
 		count++;
 	}
 	while (p_tope(pAux) != NULL) {
-		TipoElemento elementoAux = te_crear(NULL);
+		TipoElemento elementoAux = te_crear(0);
 		elementoAux = p_desapilar(pAux);
 		p_apilar(p, elementoAux);
 	}
@@ -176,24 +176,24 @@ bool p_ej3_iguales(Pila p1, Pila p2) {
 	Pila pAux = p_crear();
 	bool flag = true;
 	while (p_tope(p1) != NULL) {
-		TipoElemento elem1 = te_crear(NULL);
-		TipoElemento elem2 = te_crear(NULL);
-		TipoElemento elemAux = te_crear_con_valor(NULL, NULL);
+		TipoElemento elem1 = te_crear(0);
+		TipoElemento elem2 = te_crear(0);
+		TipoElemento elemAux = te_crear_con_valor(0, NULL);
 		if (elem1->clave != elem2->clave) {
 			flag = false;
 		}
 		elemAux->clave = elem1->clave;
-		elemAux->valor = elem2->clave;
+		elemAux->valor = (void *)(intptr_t)elem2->clave;
 		p_apilar(pAux, elemAux);
 	}
 
 	while (p_tope(pAux) != NULL) {
-		TipoElemento elemetoAux = te_crear_con_valor(NULL, NULL);
-		TipoElemento elem1 = te_crear(NULL);
-		TipoElemento elem2 = te_crear(NULL);
+		TipoElemento elemetoAux = te_crear_con_valor(0, NULL);
+		TipoElemento elem1 = te_crear(0);
+		TipoElemento elem2 = te_crear(0);
 		elemetoAux = p_desapilar(pAux);
 		elem1->clave = elemetoAux->clave;
-		elem2->clave = elemetoAux->valor;
+		elem2->clave = (int)(intptr_t)elemetoAux->valor;
 		p_apilar(p1, elem1);
 		p_apilar(p2, elem2);
 	}
@@ -201,3 +201,76 @@ bool p_ej3_iguales(Pila p1, Pila p2) {
 
 }
 
+char* p_ej4_cambiarbase(int nrobasedecimal, int nrootrabase){
+    Pila pAux = p_crear();
+    
+    while (nrobasedecimal > 0) {
+        int resto = nrobasedecimal % nrootrabase;
+        nrobasedecimal = nrobasedecimal / nrootrabase;
+        if (resto < 10) {
+            TipoElemento x = te_crear(resto);
+            p_apilar(pAux, x);
+        } else {
+            char letra = 'A' + (resto - 10);
+            char* str = (char*)malloc(2); 
+            str[0] = letra;
+            str[1] = '\0';
+            TipoElemento x = te_crear_con_valor(0, str);
+            p_apilar(pAux, x);
+        }
+    }
+    p_mostrar(pAux);
+}
+
+
+int largo(Pila p){
+    int count=0;
+    if(p_es_vacia(p)){
+        return 0;
+    }
+    Pila pAux=p_crear();
+    while (p_tope(p)!=NULL)
+    {
+        TipoElemento aux=te_crear_con_valor(0,NULL);
+        aux=p_desapilar(p);
+        p_apilar(pAux,aux);
+        count++;
+    }
+    while (p_tope(pAux)!=NULL)
+    {
+        TipoElemento aux=te_crear_con_valor(0,NULL);
+        aux=p_desapilar(pAux);
+        p_apilar(p,aux);
+    }
+
+    return count;
+}
+
+char* pasarString(Pila p){
+    
+    int l=largo(p);
+    Pila pAux=p_crear();
+    char *texto = (char *)malloc( l* sizeof(char));
+
+    while(p_tope(p)!=NULL){
+        TipoElemento elemento=te_crear_con_valor(0,NULL);
+        elemento=p_desapilar(p);
+        
+        if(elemento->valor==NULL){
+            char str[10];  
+            sprintf(str, "%d", elemento->clave);
+            strcpy(texto, str);
+        }else{
+            strcpy(texto, elemento->valor);
+        }
+        p_apilar(pAux,elemento);
+    }
+    while (p_tope(pAux)!=NULL)
+    {
+        TipoElemento aux=te_crear_con_valor(0,NULL);
+        aux=p_desapilar(pAux);
+        p_apilar(p,aux);
+    }
+
+    return texto;
+}
