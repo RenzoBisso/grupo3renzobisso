3d0b76b46e637bb0754adf9d025a554baafc51e5
Ninnerman <127340604+Ninnerman@users.noreply.github.com>
2025-04-23 20:41:17 -0300

    Punto 3

--- programacion2-2025/03-trabajoPractico-Pilas/src/main.c
+++ programacion2-2025/03-trabajoPractico-Pilas/src/main.c
@@ -6,26 +6,7 @@ int main(){
 
     srand(time(NULL));
 
-    //menu();
-
-    char* texto = malloc(100 * sizeof(char));
-
-    texto = p_ej4_cambiarbase(10, 2);
-
-    printf("%s\n", texto);
-    printf("Prueba\n");
-
-    Pila p=p_crear();
-    p_cargar(p);
-
-    p_mostrar(p_ej5_invertir(p));
-    Pila p2=p_crear();
-    Pila pAux2=p_crear();
-
-    p_cargar(p2);
-    pAux2=p_ej6_eliminarclaveR(p2,1);
-    p_mostrar(pAux2);
-    pausa();
+    menu();
 
     return 0;
 }
