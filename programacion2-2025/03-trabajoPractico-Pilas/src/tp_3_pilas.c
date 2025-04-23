1b0a34ca5bd2a0001c0c760aa2fb20dbddecadbe
Renzo Bisso <renzo_bisso@outlook.com>
2025-04-23 17:20:57 -0300

    ejercicios d,e,f y 3

--- programacion2-2025/03-trabajoPractico-Pilas/src/tp_3_pilas.c
+++ programacion2-2025/03-trabajoPractico-Pilas/src/tp_3_pilas.c
@@ -70,7 +70,134 @@ Pila p_ej2_eliminarclave(Pila p, int clave){
     return p;
 }
 
-//D
-Pila p_ej2_intercambiarposiciones(Pila p, int pos1, int pos2){
-    
+
+Pila p_ej2_intercambiarposiciones(Pila p, int pos1, int pos2) {
+	if (p_es_vacia(p)) {
+		return p;
+	}
+	int posActual = 1;
+	Pila pAux = p_crear();
+	TipoElemento elemento1 = te_crear(NULL);
+	TipoElemento elemento2 = te_crear(NULL);
+
+
+	//buscamos los elementos en las posiciones dadas y guardamos en la pila auxiliar
+	while (p_tope(p) != NULL) {
+		TipoElemento elementoAux = te_crear(NULL);
+		if (posActual == pos1) {
+			elemento1 = p_desapilar(p);
+			p_apilar(pAux, elemento1);
+			posActual++;
+		}
+		else if (posActual == pos2) {
+			elemento2 = p_desapilar(p);
+			p_apilar(pAux, elemento2);
+			posActual++;
+		}
+		else {
+			elementoAux = p_desapilar(p);
+			p_apilar(pAux, elementoAux);
+			posActual++;
+		}
+	}
+	//Vamos restando el contador hasta que llegamos a la posicion dada y apilamos el valor correspondiente
+	while (p_tope(pAux) != NULL) {
+		TipoElemento elementoAux = te_crear(NULL);
+		if (posActual == pos1) {
+			p_desapilar(pAux);
+			p_apilar(p, elemento2);
+			posActual--;
+		}
+		else if (posActual == pos2) {
+			p_desapilar(pAux);
+			p_apilar(p, elemento1);
+			posActual--;
+		}
+		else {
+			elementoAux = p_desapilar(pAux);
+			p_apilar(p, elementoAux);
+			posActual--;
+		}
+	}
+	return p;
 }
+
+Pila p_ej2_duplicar(Pila p) {
+	if (p_es_vacia(p)) {
+		return p;
+	}
+	Pila p2 = p_crear();
+	Pila pAux = p_crear();
+
+	while (p_tope(p) != NULL) {
+		TipoElemento elementoAux = te_crear(NULL);
+		elementoAux = p_desapilar(p);
+		p_apilar(pAux, elementoAux);
+	}
+	while (p_tope(pAux) != NULL) {
+		TipoElemento elementoAux = te_crear(NULL);
+		elementoAux = p_desapilar(pAux);
+		p_apilar(p, elementoAux);
+		p_apilar(p2, elementoAux);
+	}
+	return p2;
+}
+
+
+int p_ej2_cantidadelementos(Pila p) {
+	if (p_es_vacia(p)) {
+		return 0;
+	}
+	Pila pAux = p_crear();
+	int count = 0;
+	while (p_tope(p) != NULL) {
+		TipoElemento elementoAux = te_crear(NULL);
+		elementoAux = p_desapilar(p);
+		p_apilar(pAux, elementoAux);
+		count++;
+	}
+	while (p_tope(pAux) != NULL) {
+		TipoElemento elementoAux = te_crear(NULL);
+		elementoAux = p_desapilar(pAux);
+		p_apilar(p, elementoAux);
+	}
+
+	return count;
+}
+
+bool p_ej3_iguales(Pila p1, Pila p2) {
+	if (p_es_vacia(p1) || p_es_vacia(p2)) {
+		return false;
+	}
+
+	if ((p_ej2_cantidadelementos(p1) > p_ej2_cantidadelementos(p2)) || (p_ej2_cantidadelementos(p1) < p_ej2_cantidadelementos(p2))) {
+		return false;
+	}
+	Pila pAux = p_crear();
+	bool flag = true;
+	while (p_tope(p1) != NULL) {
+		TipoElemento elem1 = te_crear(NULL);
+		TipoElemento elem2 = te_crear(NULL);
+		TipoElemento elemAux = te_crear_con_valor(NULL, NULL);
+		if (elem1->clave != elem2->clave) {
+			flag = false;
+		}
+		elemAux->clave = elem1->clave;
+		elemAux->valor = elem2->clave;
+		p_apilar(pAux, elemAux);
+	}
+
+	while (p_tope(pAux) != NULL) {
+		TipoElemento elemetoAux = te_crear_con_valor(NULL, NULL);
+		TipoElemento elem1 = te_crear(NULL);
+		TipoElemento elem2 = te_crear(NULL);
+		elemetoAux = p_desapilar(pAux);
+		elem1->clave = elemetoAux->clave;
+		elem2->clave = elemetoAux->valor;
+		p_apilar(p1, elem1);
+		p_apilar(p2, elem2);
+	}
+	return flag;
+
+}
+
